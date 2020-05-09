c09cdba8ae6734ab0a78315ec4d807bb0ccb63e3
therawcoder <56636747+therawcoder@users.noreply.github.com>
2020-05-09 20:48:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+var name =prompt("Hello, What's your Name ?");
+var first =name.slice(0,1).toUpperCase();
+var second = name.slice(1,name.length).toLowerCase();
+alert("Welcome , "+first+second +"!! to my website");
